d19bab5dddca89a5bce4ac6445e94260e418e8b5
BoffinWritesCode <reachjoshjohnson@gmail.com>
2022-03-03 14:34:03 +0000

    fixed formatting

--- solutions.rb
+++ solutions.rb
@@ -3,31 +3,31 @@
 # -----------------------------------------------------------------
 
 def get_interesting(number, awesome_phrases)
-	num_s = number.to_s
-	chars = num_s.chars
-		return false if number < 100
-		# a digit followed by zeroes
-	return true if chars.count("0") == chars.length - 1
-		#all same
-	return true if chars.uniq.length <= 1
-		#all sequential incrementing
-	return true if chars.select.with_index { |c, i| i == 0 || chars[i].to_i == (chars[i-1].to_i + 1) % 10 }.length == chars.length
-		#all sequential decrementing
-	return true if chars.select.with_index { |c, i| i == 0 || chars[i].to_i == chars[i-1].to_i - 1 }.length == chars.length
-		#palindromic
-	return true if num_s.reverse == num_s
-		#in phrases
-	return true if awesome_phrases.include?(number)
-		false
+    num_s = number.to_s
+    chars = num_s.chars
+        return false if number < 100
+        # a digit followed by zeroes
+    return true if chars.count("0") == chars.length - 1
+        #all same
+    return true if chars.uniq.length <= 1
+        #all sequential incrementing
+    return true if chars.select.with_index { |c, i| i == 0 || chars[i].to_i == (chars[i-1].to_i + 1) % 10 }.length == chars.length
+        #all sequential decrementing
+    return true if chars.select.with_index { |c, i| i == 0 || chars[i].to_i == chars[i-1].to_i - 1 }.length == chars.length
+        #palindromic
+    return true if num_s.reverse == num_s
+        #in phrases
+    return true if awesome_phrases.include?(number)
+        false
 end
 
 def is_interesting(number, awesome_phrases)
-	if get_interesting(number, awesome_phrases)
-		return 2
-	elsif get_interesting(number + 1, awesome_phrases) || get_interesting(number + 2, awesome_phrases)
-		return 1
-	end
-	0
+    if get_interesting(number, awesome_phrases)
+        return 2
+    elsif get_interesting(number + 1, awesome_phrases) || get_interesting(number + 2, awesome_phrases)
+        return 1
+    end
+    0
 end
 
 # https://www.codewars.com/kata/52b7ed099cdc285c300001cd
@@ -35,26 +35,26 @@ end
 # -----------------------------------------------------------------
 
 def sum_of_intervals(intervals)
-	ranges = []
+    ranges = []
     # for each interval
-	intervals.each { |arr| 
+    intervals.each { |arr| 
         # get the range
-		range = arr[0]..arr[1]
+        range = arr[0]..arr[1]
         # calculate any overlaps, by selecting ranges which overlap with this range
-		overlaps = ranges.select{ |r| !(r.first > range.last || r.last < range.first) }
+        overlaps = ranges.select{ |r| !(r.first > range.last || r.last < range.first) }
         # if any overlaps
-		if overlaps.length > 0 then
+        if overlaps.length > 0 then
             # remove the overlaps from the list of ranges
-			ranges -= overlaps
+            ranges -= overlaps
             # get the new range, based on the overlap
-			overlaps.push(range)
-			min = overlaps.map(&:first).min
-			max = overlaps.map(&:last).max
-			range = min..max
-		end
+            overlaps.push(range)
+            min = overlaps.map(&:first).min
+            max = overlaps.map(&:last).max
+            range = min..max
+        end
         # push the range
         ranges.push(range)
-	}
+    }
     # get the sum of all ranges
     ranges.sum { |r| r.last - r.first }
 end
@@ -64,44 +64,44 @@ end
 # -----------------------------------------------------------------
 
 def triangle(row)
-	next_row(row, row.length)
+    next_row(row, row.length)
 end
 
 def next_row(row, length)
-	return row[0] if length == 1
+    return row[0] if length == 1
     length.times { |i| 
-		current = row[i]
-		next = row[i + 1]
-		row[i] = current == next ? current : get_next(current, next)
-	}
-	next_row(row, length - 1)
+        current = row[i]
+        next = row[i + 1]
+        row[i] = current == next ? current : get_next(current, next)
+    }
+    next_row(row, length - 1)
 end
 
 # horrid function, but gets the correct letter based on a's value and b's value
 def get_next(a, b)
-	case a
-	when "R"
-		case b
-		when "G"
-			"B"
-		else
-			"G"
-		end
-	when "G"
-		case b
-		when "B"
-			"R"
-		else
-			"B"
-		end
-	when "B"
-		case b
-		when "R"
-			"G"
-		else
-			"R"
-		end
-	end
+    case a
+    when "R"
+        case b
+        when "G"
+            "B"
+        else
+            "G"
+        end
+    when "G"
+        case b
+        when "B"
+            "R"
+        else
+            "B"
+        end
+    when "B"
+        case b
+        when "R"
+            "G"
+        else
+            "R"
+        end
+    end
 end
 
 # https://www.codewars.com/kata/54b72c16cd7f5154e9000457
@@ -110,35 +110,35 @@ end
 # preloaded: MORSE_CODE
 
 def decodeBits(bits)
-	print bits
-	sections = bits.scan(/0+|1+/)
-	rate = sections.select.with_index{ |k, i| k[0] == "0" ? i > 0 && i < sections.length - 1 : true }.map(&:length).min
-	sections.map { |sec|
-		parts = (sec.length / rate).floor
-		case sec[0]
-		when "1"
-			case parts
-			when 1
-				"."
-			when 3
-				"-"
-			end
-		else
-			case parts
-			when 3
-				" "
-			when 7
-				"   "
-			end
-		end
-	}.join
+    print bits
+    sections = bits.scan(/0+|1+/)
+    rate = sections.select.with_index{ |k, i| k[0] == "0" ? i > 0 && i < sections.length - 1 : true }.map(&:length).min
+    sections.map { |sec|
+        parts = (sec.length / rate).floor
+        case sec[0]
+        when "1"
+            case parts
+            when 1
+                "."
+            when 3
+                "-"
+            end
+        else
+            case parts
+            when 3
+                " "
+            when 7
+                "   "
+            end
+        end
+    }.join
 end
 
 def decodeMorse(morseCode)
-	print morseCode
-	morseCode.strip.split('   ').map { |word|
-		word.split(' ').map { |morse_seq| MORSE_CODE[morse_seq] }.join
-	}.join(' ')
+    print morseCode
+    morseCode.strip.split('   ').map { |word|
+        word.split(' ').map { |morse_seq| MORSE_CODE[morse_seq] }.join
+    }.join(' ')
 end
 
 # https://www.codewars.com/kata/54b724efac3d5402db00065e
@@ -146,9 +146,9 @@ end
 # -----------------------------------------------------------------
 
 def decodeMorse(morseCode)
-	morseCode.strip.split('   ').map { |word|
-		word.split(' ').map { |morse_seq| MORSE_CODE[morse_seq] }.join
-	}.join(' ')
+    morseCode.strip.split('   ').map { |word|
+        word.split(' ').map { |morse_seq| MORSE_CODE[morse_seq] }.join
+    }.join(' ')
 end
 
 # https://www.codewars.com/kata/52742f58faf5485cae000b9a 
